3a1e0b0c6bd379de42fe9dbfc24f08a874399844
Jomanahani <jomanahani123456@gmail.com>
2022-12-25 15:13:36 +0200

    create file structre for all psges i have also added some components and assetse folder

---
+++ src/Component/IntroSide/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './style.css'
+
+export default class IntroSide extends Component {
+  render() {
+    return (
+      <div className='introduction'>
+            <p>I always observe the people who pass by when I ride an escalator.
+                 I'll never see most of them again, so I imagine a lot of things 
+                 about their lives... about the day ahead of them.</p>
+      </div>
+    )
+  }
+}
---
+++ src/Pages/SingUp/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Layout from '../../Component/Layout/index'
+
+class SingUp extends Component {
+  render() {
+    return (
+        <Layout>
+
+        </Layout>
+    )
+  }
+}
+export default SingUp
