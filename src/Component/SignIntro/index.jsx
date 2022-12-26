55ab1a498b20199d24cb81307017d425d325dc9e
Jomanahani <jomanahani123456@gmail.com>
2022-12-26 16:59:46 +0200

    design signup page and add the background to the left side of the page

---
+++ src/Component/SignIntro/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './style.css'
+
+export default class SignIntro extends Component {
+  render() {
+    return (
+      <div className='signIntro'>
+        <div>
+            
+        </div>
+      </div>
+    )
+  }
+}
--- src/Pages/SingUp/index.jsx
+++ src/Pages/SingUp/index.jsx
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import Layout from '../../Component/Layout/index'
+import Titles from "../../Component/Titles";
+import IntroSide from '../../Component/IntroSide/index'
+import FormSide from '../../Component/FormSide/index'
+import SignIntro from '../../Component/SignIntro/index'
 
 class SingUp extends Component {
   render() {
     return (
-        <Layout>
-
-        </Layout>
-    )
+      <div>
+      <Layout>
+        <IntroSide>
+          <SignIntro />
+        </IntroSide>
+        <FormSide>  
+      <Titles
+        title="Register Individual Account!"
+        supTitle="For the purpose of gamers regulation, your details are required."
+      />
+        </FormSide>
+      </Layout>
+      </div>
+    );
   }
 }
-export default SingUp
+export default SingUp;
