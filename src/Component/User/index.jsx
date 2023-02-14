e98d1d1ceeddbe6ee1009d3dc526ee8000b17bf7
Jomanahani <jomanahani123456@gmail.com>
2023-02-14 21:00:15 +0200

    add users list

--- src/Component/User/index.jsx
+++ src/Component/User/index.jsx
@@ -1,31 +1,63 @@
-import React, { Component } from 'react'
-import Profile from '../../assetse/Profile.png'
-import {TbLogout} from 'react-icons/tb'
-import './style.css'
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { API_URL } from "../../config/api";
+
+import Avatar from "../../assetse/Avatar.png";
+
+import { TbLogout } from "react-icons/tb";
+import { FaUsers } from "react-icons/fa";
+
+import "./style.css";
 
 export default class User extends Component {
-  state = { userName: "" };
+  state = {
+    userName: "",
+    admin: "",
+    isLoading: true,
+  };
 
   async componentDidMount() {
-    const userName = localStorage.getItem("userName");
-    this.setState({ userName });
+    const token = localStorage.getItem("token");
+    const res = await axios.get(`${API_URL}/users/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    this.setState({
+      userName: res.data.name,
+      admin: res.data.isAdmin,
+      isLoading: false,
+    });
   }
 
   render() {
     return (
-      <div className='profile'>
+      <div className="profile">
         <div>
-        <div className='info'>
-        <h2>Welcome back,</h2>
-        <h2>{this.state.userName}</h2>
-        </div>
-        <Link to='/profile'>
-        <img  title='profile' src={Profile} alt='Profile'/>
-        </Link>
-        <TbLogout title='logout' className='icon' onClick={this.props.logout}/>
+          <div className="info">
+            <h2>Welcome back,</h2>
+            <h2>{this.state.userName}</h2>
+          </div>
+          <Link to="/profile">
+            <img title="profile" src={Avatar} alt="Profile" />
+          </Link>
+          {this.state.admin ? (
+            <Link to="/userlist" className="userslist">
+              <FaUsers />
+              <p>Users List</p>
+            </Link>
+          ) : (
+            ""
+          )}
+          <TbLogout
+            title="logout"
+            className="icon"
+            onClick={this.props.logout}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -5,9 +5,11 @@ import { API_URL } from "./../../config/api";
 import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 
-import profile from "../../assetse/profile.jpg";
+import Avatar from "../../assetse/Avatar.png";
 
 import "./style.css";
+import User from "../../Component/User";
+import SideBar from "../../Component/SideBar";
 
 export default class Profile extends Component {
   state = {
@@ -35,35 +37,32 @@ export default class Profile extends Component {
 
   render() {
     return (
-      <>
-        <Link to="/home">
-          <p className="Back">
-            <IoIosArrowBack />
-            Back
-          </p>
-        </Link>
-        <div className="account">
-          <img src={profile} alt="profile" />
-          <h1>{this.state.userName}s Profile</h1>
-          {this.state.isLoading ? (
-            "Loading..."
-          ) : (
-            <div className="profile_info">
-              <p>Name:</p>
-              <p className="user_info">{this.state.userName}</p>
-              <p>Email:</p>
-              <p className="user_info">{this.state.email}</p>
-            </div>
-          )}
-          {/* {this.state.admin ? (
-            <NavLink to="/userslist" className={"userslist"}>
-              Users List
-            </NavLink>
-          ) : (
-            ""
-          )} */}
-        </div>
-      </>
+      <div className="home">
+        <SideBar />
+        <main className="usersMain">
+          <User />
+          <Link to="/" className="Back">
+            <p>
+              <IoIosArrowBack />
+              Back
+            </p>
+          </Link>
+          <div className="account">
+            <img src={Avatar} alt="profile" />
+            <h1>{this.state.userName}s Profile</h1>
+            {this.state.isLoading ? (
+              "Loading..."
+            ) : (
+              <div className="profile_info">
+                <p>Name:</p>
+                <p className="user_info">{this.state.userName}</p>
+                <p>Email:</p>
+                <p className="user_info">{this.state.email}</p>
+              </div>
+            )}
+          </div>
+        </main>
+      </div>
     );
   }
 }
